86d8bb55628dee9a0bf78681bdc521d53d8301e9
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2026-03-12 02:01:41 +0300

    add utill

--- wine_butik_ru_spider/util.py
+++ wine_butik_ru_spider/util.py
@@ -2,19 +2,19 @@ import logging
 from urllib import parse
 
 
-WINE_CARDS_XPATH = "//div[@class='catalog-item']"
-WINE_HREFS_XPATH = ".//div[@class='catalog-item__desc']/h2/a[@href]/@href"
-NEXT_PAGE_XPATH = "//div[@class='paging_box']/.//a[@class='next']/@value"
-WINE_NAME1_XPATH = "//div[contains(@class, 'product-translation')]/span/text()[normalize-space()]"
-WINE_NAME2_XPATH = "//div[contains(@class, 'product-title')]/h1/text()[normalize-space()]"
-VINTAGE1_XPATH = "//div[contains(@class, 'prod_param_2')]/.//option[@selected]/text()"
-VINTAGE2_XPATH = "//div[contains(@class, 'prod_param_2')]/p/em[contains(text(), 'Год')]/../text()"
-AVAILABILITY_XPATH = "//div[contains(@class, 'product-shop wait')]/@class | //div[contains(@class, 'product-shop avail')]/@class"
-IMG_URL_XPATH = "//img[contains(@class, 'product-image-large')]/@src"
-PRICE_XPATH = "//div[contains(@id, 'product-price')]/div[2]/text()"
-CURRENCY_XPATH = "//div[contains(@id, 'product-price')]/div[3]/text()"
-VOLUME1_XPATH = "//div[@class='product-params']/p/em[contains(text(), 'Объем')]/../text()"
-VOLUME2_XPATH = "//div[@class='product-params']/p/em[contains(text(), 'Объем')]/..//option[@selected]/text()"
+WINE_CARDS_XPATH: str = "//div[@class='catalog-item']"
+WINE_HREFS_XPATH: str = ".//div[@class='catalog-item__desc']/h2/a[@href]/@href"
+NEXT_PAGE_XPATH: str = "//div[@class='paging_box']/.//a[@class='next']/@value"
+WINE_NAME1_XPATH: str = "//div[contains(@class, 'product-translation')]/span/text()[normalize-space()]"
+WINE_NAME2_XPATH: str = "//div[contains(@class, 'product-title')]/h1/text()[normalize-space()]"
+VINTAGE1_XPATH: str = "//div[contains(@class, 'prod_param_2')]/.//option[@selected]/text()"
+VINTAGE2_XPATH: str = "//div[contains(@class, 'prod_param_2')]/p/em[contains(text(), 'Год')]/../text()"
+AVAILABILITY_XPATH: str = "//div[contains(@class, 'product-shop wait')]/@class | //div[contains(@class, 'product-shop avail')]/@class"
+IMG_URL_XPATH: str = "//img[contains(@class, 'product-image-large')]/@src"
+PRICE_XPATH: str = "//div[contains(@id, 'product-price')]/div[2]/text()"
+CURRENCY_XPATH: str = "//div[contains(@id, 'product-price')]/div[3]/text()"
+VOLUME1_XPATH: str = "//div[@class='product-params']/p/em[contains(text(), 'Объем')]/../text()"
+VOLUME2_XPATH: str = "//div[@class='product-params']/p/em[contains(text(), 'Объем')]/..//option[@selected]/text()"
 
 
 
@@ -29,7 +29,7 @@ def availability_convert(availability: str) -> str:
     return availability
 
 
-def image_url_conver(base_url, image_url):
+def image_url_conver(base_url, image_url) -> str:
     if image_url:
         # -- выделим path
         # image_url = response.urljoin(image_url)
@@ -38,7 +38,7 @@ def image_url_conver(base_url, image_url):
         image_url = parse.urljoin(base_url, path)
     return image_url
 
-def volume_convert(volume):
+def volume_convert(volume) -> str:
     match volume.split():
         case [v, _]:
             try:
